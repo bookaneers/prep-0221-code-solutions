3705ad052b2b7e7669d6531ce6381b32cebb8114
Fernando Figueiredo <bookaneers@gmail.com>
2021-02-26 18:16:02 -0800

    Completed prep-javascript-for-loops.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,12 @@
+// create your loops here.
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (var j = 0; j <= 9; j++) {
+  console.log(j * 2);
+}
+
+for (var k = 100; k > 0; k--) {
+  console.log('Time till explosion: ' + k + '!');
+}
